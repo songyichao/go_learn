e659cb284dfc58daf6b481b22df0c36897210f02
songyichao <songyichao@mioji.com>
2018-02-24 16:01:13 +0800

    fix method

---
+++ methods-with-pointer-receivers.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X *= f
+	v.Y *= f
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+func main() {
+	v := &Vertex{3, 4}
+	v.Scale(5)
+	fmt.Println(v, v.Abs())
+}
