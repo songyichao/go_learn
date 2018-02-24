c27bed483e983f0a052706e4dbb40ea2b4c061ee
songyichao <songyichao@mioji.com>
2018-02-24 16:27:40 +0800

    fix interface

---
+++ interfaces-are-satisfied-implicitly.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+type Reader interface {
+	Read(b []byte) (n int, err error)
+}
+type Writer interface {
+	Write(b []byte) (n int, err error)
+}
+type ReadWriter interface {
+	Reader
+	Writer
+}
+
+func main() {
+	var w Writer
+	w = os.Stdout
+	fmt.Fprintf(w, "hello writer\n")
+}
